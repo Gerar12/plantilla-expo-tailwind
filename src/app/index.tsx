be8c4257f965482cdc6baf1a140f29c759ec366a
Gerar Arevalo <gerar@duck.com>
2025-02-24 19:05:48 -0600

    Actualizar el texto de presentación en la aplicación

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ const App = () => {
     <SafeAreaView className="flex-1 bg-black/90">
       <View className="flex-1 justify-center items-center">
         <Text className="text-center text-blue-200 text-4xl font-work-black">
-          {"< G CODE />"}
+          {"< G CODER />"}
         </Text>
       </View>
     </SafeAreaView>
